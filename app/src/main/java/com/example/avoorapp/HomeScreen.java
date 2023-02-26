b8e4689976f3292608d79f801fe1fdde9f6000ab
Ramachandran S <kaarthiksram55@gmail.com>
2023-02-26 23:20:08 +0530

    Home screen updates 1. Added list view to print all available menu items. Menu item names present in strings xml file. 2. Added three-dot menu for Logout and About menus. For this, made a menu xml resource file. Also updated the themes (both light and dark) to not have an action bar by default. The toolbar custom made by us will be used as the default action bar. This will house the three-dot menu.
    The three-dot menu item names should be made in strings xml. Also, onClickListener of the menu items of the list view as well as the three-dot menu to be implemented.

--- app/src/main/java/com/example/avoorapp/HomeScreen.java
+++ app/src/main/java/com/example/avoorapp/HomeScreen.java
@@ -2,43 +2,61 @@ package com.example.avoorapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-/* This class represents the Home Screen which appears when the app is just opened. */
+/* This class represents the home screen which appears when the app is just opened. */
 public class HomeScreen extends AppCompatActivity
 {
-
-    /* member variables */
-    TextView tvHomeScreenTxtViewAppName, tvHomeScreenTxtViewTitleMessage, tvHomeScreenTxtViewUserInfo, tvHomeScreenTxtViewPrdshPrtc;
-    ListView lvHomeScreenListViewMenuItems;
-
     /* This method is called in the background. Set the screen (xml layout) this class is supposed
      * to display and initialize class variables and screen items as desired. */
     public void onCreate(Bundle savedInstanceState)
     {
+        TextView tvHomeScreenTxtViewAppName, tvHomeScreenTxtViewTitleMessage, tvHomeScreenTxtViewUserInfo, tvHomeScreenTxtViewPrdshPrtc;
+        ListView lvHomeScreenListViewMenuItems;
+        Toolbar tbHomeScreenToolbar;
+        String[] strListViewMenuItemsNames =
+        {
+            this.getResources().getString(R.string.MenuItemUsersList),
+            this.getResources().getString(R.string.MenuItemPradoshamDates),
+            this.getResources().getString(R.string.MenuItemSankalpamDetails),
+            this.getResources().getString(R.string.MenuItemPhotosAndVideos)
+        };
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_screen);
-        getSupportActionBar().hide();
 
         tvHomeScreenTxtViewAppName = findViewById(R.id.HomeScreenTxtViewAppName);
         tvHomeScreenTxtViewTitleMessage = findViewById(R.id.HomeScreenTxtViewTitleMessage);
         tvHomeScreenTxtViewUserInfo = findViewById(R.id.HomeScreenTxtViewUserInfo);
         tvHomeScreenTxtViewPrdshPrtc = findViewById(R.id.HomeScreenTxtViewPrdshPrtc);
         lvHomeScreenListViewMenuItems = findViewById(R.id.HomeScreenListViewMenuItems);
+        tbHomeScreenToolbar = findViewById(R.id.HomeScreenToolbar);
 
-        tvHomeScreenTxtViewAppName.setText(R.string.AvoorAppDisplayNameEnglish);
-        tvHomeScreenTxtViewTitleMessage.setText(R.string.HomeScreenTitleEnglish);
-        tvHomeScreenTxtViewPrdshPrtc.setText("Next: 27th Feb 2023");
+        tvHomeScreenTxtViewAppName.setText(this.getResources().getString(R.string.AvoorAppDisplayNameEnglish));
+        tvHomeScreenTxtViewTitleMessage.setText(this.getResources().getString(R.string.HomeScreenTitleEnglish));
+        tvHomeScreenTxtViewPrdshPrtc.setText("Next on: 27th Feb 2023");
+        lvHomeScreenListViewMenuItems.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strListViewMenuItemsNames));
+        setSupportActionBar(tbHomeScreenToolbar);
 
+        /* The mobile number received from the previous screen will be valid as checks have been
+         * performed there. Hence no need to check here once again. */
         Intent intent = getIntent();
-        long longMobileNumber = intent.getLongExtra(String.valueOf(R.string.LandingScreenIntentUserNameKey), 0);
-        if (longMobileNumber != 0)
-        {
-            tvHomeScreenTxtViewUserInfo.setText("Welcome, " + longMobileNumber);
-        }
+        long longMobileNumber = intent.getLongExtra(this.getResources().getString(R.string.LandingScreenIntentUserNameKey), 0);
+        tvHomeScreenTxtViewUserInfo.setText("Welcome, " + longMobileNumber);
+    }
+
+    /* This method has been implemented to generate the ellipsis (three dots) menu. */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.home_screen_ellipsis_menu, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/avoorapp/LandingScreen.java
+++ app/src/main/java/com/example/avoorapp/LandingScreen.java
@@ -2,17 +2,16 @@ package com.example.avoorapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-/* This class represents the Landing Screen which appears when the app is just opened. */
+/* This class represents the landing screen which appears when the app is just opened. */
 public class LandingScreen extends AppCompatActivity
 {
-
-    /* memer variables */
+    /* member variables */
     EditText etLandingScreenEditTxtUsername, etLandingScreenEditTxtPassword;
 
     /* This method is called in the background. Set the screen (xml layout) this class is supposed
@@ -21,32 +20,41 @@ public class LandingScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_screen);
-        getSupportActionBar().hide();
 
         etLandingScreenEditTxtUsername = findViewById(R.id.LandingScreenEditTxtUsername);
         etLandingScreenEditTxtPassword = findViewById(R.id.LandingScreenEditTxtPassword);
-        etLandingScreenEditTxtPassword.setVisibility(View.INVISIBLE);
     }
 
     /* This method is called when the button to jump to home screen is clicked. This is for debug
     * purposes. */
     public void openHomeScreen(View v)
     {
+        /* Verification of login credentials:
+         *
+         * Check if the mobile number is of 10 digits. Since its type is set as 'number' in the xml
+         * layout, other checks need not be performed here. Commas, dashes, decimal points and
+         * spaces seem to be not allowed by the keypad itself even though the key is provided. This
+         * automatically checks that the field is not empty.
+         *
+         * If credentials are invalid, display an alert indicating the same. */
         String strUserNameFieldValue = String.valueOf(etLandingScreenEditTxtUsername.getText());
 
-        /* check if the username field is not empty. Since its type is set as 'number' in the xml
-         * layout, other checks need not be performed here. Commas, dashs, decimal points and
-         * spaces seem to be not allowed by the keypad itself even though the key is provided. */
-        if (!strUserNameFieldValue.isEmpty())
+        if (strUserNameFieldValue.length() == 10)
         {
             /* Make an intent object with the below flags set to prevent coming back to the landing
              * screen from home screen. Pass the mobile number information to the home screen. */
             Intent intent = new Intent(v.getContext(), HomeScreen.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            String strIntentMobileNumberKey = String.valueOf(R.string.LandingScreenIntentUserNameKey);
+            String strIntentMobileNumberKey = this.getResources().getString(R.string.LandingScreenIntentUserNameKey);
             long longMobileNumberValue = Long.parseLong(strUserNameFieldValue);
             intent.putExtra(strIntentMobileNumberKey, longMobileNumberValue);
             startActivity(intent);
+        }
+        else
+        {
+            AlertDialog adLoginStatusDialog = new AlertDialog.Builder(this).create();
+            adLoginStatusDialog.setMessage(this.getResources().getString(R.string.LandingScreenInvalidLogin));
+            adLoginStatusDialog.show();
         }
     }
 }
