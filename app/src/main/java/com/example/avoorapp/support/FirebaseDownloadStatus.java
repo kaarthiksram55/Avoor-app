d23e82a38bdda411bc531efae65882966e9aebf1
Ramachandran S <kaarthiksram55@gmail.com>
2023-03-12 17:26:30 +0530

    Unstable commit. Firebase integration under progress. Missed out some files in last commit.

---
+++ app/src/main/java/com/example/avoorapp/support/FirebaseDownloadStatus.java
@@ -0,0 +1,12 @@
+package com.example.avoorapp.support;
+
+/* Implement an interface to store data downloaded from firebase in async task. */
+public interface FirebaseDownloadStatus {
+    /* This function can be implemented as desired to retrieve data from async task of the firestore
+     * class used in the FirebaseWrapper custom class. */
+    void onDownloadCompleteCallback();
+
+    /* This function can be implemented as desired to take actions in case asynchronous download of
+     * data from firestore failed.  */
+    void onDownloadFailureCallback();
+}
