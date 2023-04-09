ca3ea66b4aa513a0e2fa9f39696a9cc364211ca1
Ramachandran S <kaarthiksram55@gmail.com>
2023-04-09 08:17:49 +0530

    Updated in pradosham screen. 1. A dropdown has now been provided to select the year. Based on this selection, the table will show the pradosham info of the selected year. 2. The table headers have been moved from the layout xml file to the java class. Since the previous rows of the table had to be removed, the headers were being removed as well. So now they will always be inserted programatically.

--- app/src/main/java/com/example/avoorapp/PradoshamDatesScreen.java
+++ app/src/main/java/com/example/avoorapp/PradoshamDatesScreen.java
@@ -1,6 +1,12 @@
 package com.example.avoorapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -21,6 +27,7 @@ import androidx.appcompat.widget.Toolbar;
 public class PradoshamDatesScreen extends AppCompatActivity {
     /* member variables */
     private TableLayout tblPradoshamDatesScreenPradoshamInfoTable;
+    private Spinner spnPradoshamDatesScreenYearDropdown;
     private FirebaseWrapper tempWrapper;
     private ArrayList<PradoshamInfo> pradoshamInfoList;
     private CustomAlertDialog alertDialog;
@@ -37,6 +44,7 @@ public class PradoshamDatesScreen extends AppCompatActivity {
         tblPradoshamDatesScreenPradoshamInfoTable = findViewById(R.id.PradoshamDatesScreenPradoshamInfoTable);
         tvPradoshamDatesScreenTxtViewAppName = findViewById(R.id.PradoshamDatesScreenTxtViewAppName);
         tvPradoshamDatesScreenTxtViewTitleMessage = findViewById(R.id.PradoshamDatesScreenTxtViewTitleMessage);
+        spnPradoshamDatesScreenYearDropdown = findViewById(R.id.PradoshamDatesScreenYearDropdown);
         tlbarPradoshamDatesScreenToolbar = findViewById(R.id.PradoshamDatesScreenToolbar);
         setSupportActionBar(tlbarPradoshamDatesScreenToolbar);
 
@@ -64,7 +72,8 @@ public class PradoshamDatesScreen extends AppCompatActivity {
             public void onDownloadCompleteCallback()
             {
                 pradoshamInfoList = tempWrapper.getPradoshamDetails();
-                loadPradoshamInfoTableWithInfo(pradoshamInfoList);
+                loadYearSelectDropdownWithInfo();
+//                loadPradoshamInfoTableWithInfo(pradoshamInfoList);
             }
 
             @Override
@@ -75,15 +84,81 @@ public class PradoshamDatesScreen extends AppCompatActivity {
         });
     }
 
-    /* This private function will load the table in the screen with the pradosham info retrieved
-     * from firebase. */
-    private void loadPradoshamInfoTableWithInfo(ArrayList<PradoshamInfo> pradoshamInfoList)
+    /* This private function will load the year select dropdown with the downloaded pradosham
+     * financial year dates. */
+    private void loadYearSelectDropdownWithInfo()
     {
-        int intSrNo = 1;
+        ArrayList<String> strYearSelectDropdownList = new ArrayList<>();
 
         for (int i=0; i<pradoshamInfoList.size(); i++)
         {
-            List<Map<String, String>> tempPradoshamDetailsList = pradoshamInfoList.get(i).getPradoshamDetails();
+            strYearSelectDropdownList.add(pradoshamInfoList.get(i).getYearName());
+        }
+
+        spnPradoshamDatesScreenYearDropdown.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, strYearSelectDropdownList));
+        spnPradoshamDatesScreenYearDropdown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                /* i corresponds to the selected index, which is in the same order as in the array
+                 * of options set to the dropdown. Pass this index so that the corresponding year's
+                 * info can be displayed in the table. */
+
+                tblPradoshamDatesScreenPradoshamInfoTable.removeAllViews();
+                loadPradoshamTableHeaders();
+                loadPradoshamInfoTableWithInfo(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                /* Do nothing here. */
+            }
+        });
+    }
+
+    /* This private function will load the table with the table headers. */
+    private void loadPradoshamTableHeaders()
+    {
+        TableRow tblrowPradoshamInfoRow = new TableRow(this);
+        tblrowPradoshamInfoRow.setLayoutParams(new TableRow.LayoutParams());
+        TextView tvPradoshamInfoCell;
+
+        tvPradoshamInfoCell = new TextView(this);
+        tvPradoshamInfoCell.setText(getApplicationContext().getResources().getString(R.string.PradoshamDatesScreenTableHeaderSNo));
+        tvPradoshamInfoCell.setPadding(10, 10, 10, 10);
+        tblrowPradoshamInfoRow.addView(tvPradoshamInfoCell);
+
+        tvPradoshamInfoCell = new TextView(this);
+        tvPradoshamInfoCell.setText(getApplicationContext().getResources().getString(R.string.PradoshamDatesScreenTableHeaderId));
+        tvPradoshamInfoCell.setPadding(10, 10, 10, 10);
+        tblrowPradoshamInfoRow.addView(tvPradoshamInfoCell);
+
+        tvPradoshamInfoCell = new TextView(this);
+        tvPradoshamInfoCell.setText(getApplicationContext().getResources().getString(R.string.PradoshamDatesScreenTableHeaderTamilDate));
+        tvPradoshamInfoCell.setPadding(10, 10, 10, 10);
+        tblrowPradoshamInfoRow.addView(tvPradoshamInfoCell);
+
+        tvPradoshamInfoCell = new TextView(this);
+        tvPradoshamInfoCell.setText(getApplicationContext().getResources().getString(R.string.PradoshamDatesScreenTableHeaderEnglishDate));
+        tvPradoshamInfoCell.setPadding(10, 10, 10, 10);
+        tblrowPradoshamInfoRow.addView(tvPradoshamInfoCell);
+
+        tvPradoshamInfoCell = new TextView(this);
+        tvPradoshamInfoCell.setText(getApplicationContext().getResources().getString(R.string.PradoshamDatesScreenTableHeaderDay));
+        tvPradoshamInfoCell.setPadding(10, 10, 10, 10);
+        tblrowPradoshamInfoRow.addView(tvPradoshamInfoCell);
+
+        tblPradoshamDatesScreenPradoshamInfoTable.addView(tblrowPradoshamInfoRow);
+    }
+
+    /* This private function will load the table in the screen with the pradosham info retrieved
+     * from firebase. */
+    private void loadPradoshamInfoTableWithInfo(int intSelectedYearIndex)
+    {
+        int intSrNo = 1;
+
+//        for (int i=0; i<pradoshamInfoList.size(); i++)
+//        {
+            List<Map<String, String>> tempPradoshamDetailsList = pradoshamInfoList.get(intSelectedYearIndex).getPradoshamDetails();
             for (int j=0; j<tempPradoshamDetailsList.size(); j++)
             {
                 Map<String, String> tempPradoshamInfoMap = tempPradoshamDetailsList.get(j);
@@ -120,6 +195,6 @@ public class PradoshamDatesScreen extends AppCompatActivity {
 
                 tblPradoshamDatesScreenPradoshamInfoTable.addView(tblrowPradoshamInfoRow);
             }
-        }
+//        }
     }
 }
